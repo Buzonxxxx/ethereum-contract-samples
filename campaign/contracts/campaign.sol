d7a668255b1c2423cf287d4a7d657c49365d7634
louis.liao <buzonliao@gmail.com>
2018-04-22 21:23:35 +0800

    modify: campaign.sol

--- campaign/contracts/campaign.sol
+++ campaign/contracts/campaign.sol
@@ -1,18 +1,20 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
-
+    //Request is a type
     struct Request {
         string description;
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping (address => bool) approvals;
     }
     
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
     
     modifier restricted() {
         require(msg.sender == manager);
@@ -26,18 +28,30 @@ contract Campaign {
     
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
-    
+        approvers[msg.sender] = true;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
+    function createRequest(string description, uint value, address recipient, uint approvalCount) public restricted {
+        // Create a variable that its type is Request
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
         
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        //確認有捐錢
+        require(approvers[msg.sender]);
+        //確認之前沒approve過
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
     }
 }
