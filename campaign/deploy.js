099afd258e1267a0305b81ee1bf779c4ecb0d61f
Louis Liao <buzonliao@gmail.com>
2018-05-07 11:58:16 +0800

    add: campaign e2e test and deploy

---
+++ campaign/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+const compiledFactory = require('./build/CampaignFactory.json')
+
+const provider = new HDWalletProvider(
+  'apple cry couch mobile wood wealth army sign betray then abstract loan',
+  'https://rinkeby.infura.io/vLyWxwMRCxh44cqIEtUy'
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts()
+  console.log('Attempting to deploy from account', accounts[0])
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: '0x' + compiledFactory.bytecode })
+    .send({ gas: '4700000', from: accounts[0] })
+  
+  console.log('Contract deployed to', result.options.address)
+}
+
+deploy()
