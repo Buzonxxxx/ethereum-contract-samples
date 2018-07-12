c023dd1548b19e8a80aa70340c231f98e4c752fb
Louis Liao <buzonliao@gmail.com>
2018-07-12 11:58:01 +0800

    modify kickstarter project structure

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -13,9 +13,7 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts()
   console.log('Attempting to deploy from account', accounts[0])
 
-  const result = await new web3.eth.Contract(
-    JSON.parse(compiledFactory.interface)
-  )
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: '0x' + compiledFactory.bytecode })
     .send({ gas: '4700000', from: accounts[0] })
   
--- kickstart/test/campaign.test.js
+++ kickstart/test/campaign.test.js
@@ -4,8 +4,8 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 //instance
 const web3 = new Web3(ganache.provider())
-const compiledCampaign = require('../build/Campaign.json')
-const compiledFactory = require('../build/CampaignFactory.json')
+const compiledCampaign = require('../ethereum/build/Campaign.json')
+const compiledFactory = require('../ethereum/build/CampaignFactory.json')
 
 let accounts
 let campaign
@@ -19,10 +19,8 @@ beforeEach(async () => {
     .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: '1000000' })
   
-  await factory.methods.createCampaign('100').send({  // 100wei
-    from: accounts[0],
-    gas: '1000000'
-  })
+  await factory.methods.createCampaign('100')
+    .send({ from: accounts[0], gas: '1000000' })
 
   const address = await factory.methods.getDeployedCampaigns().call()
   campaignAddress = address[0]
@@ -56,8 +54,7 @@ describe('Campaigns', () => {
     assert(false)
   }) 
   it('allows a manager to make a payment request', async () => {
-    await campaign.methods
-    .createRequest('Buy batteries', '100', accounts[1])
+    await campaign.methods.createRequest('Buy batteries', '100', accounts[1])
     .send({ from: accounts[0], gas: '1000000' })
     
     request = await campaign.methods.requests(0).call()
@@ -66,13 +63,10 @@ describe('Campaigns', () => {
     assert.equal(accounts[1], request.recipient)
   })
   it('processes requests', async () => {
-    await campaign.methods.contribute().send({
-      from: accounts[0],
-      value: web3.utils.toWei('10', 'ether')
-    })
+    await campaign.methods.contribute()
+    .send({ from: accounts[0], value: web3.utils.toWei('10', 'ether') })
 
-    await campaign.methods
-      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+    await campaign.methods.createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
       .send({ from: accounts[0], gas: '1000000' })
 
     await campaign.methods.approveRequest(0).send({
@@ -88,9 +82,6 @@ describe('Campaigns', () => {
     let balance = await web3.eth.getBalance(accounts[1]) //string
     balance = web3.utils.fromWei(balance, 'ether')
     balance = parseFloat(balance) 
-    // console.log(balance)
     assert(balance > 104)
-    
-    
   }) 
 })
