41eb91200a78512a76aee537b8e7fb31ec535b0c
louis.liao <buzonliao@gmail.com>
2018-07-17 23:27:11 +0800

    modify: CampaignShow

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
+import Layout from '../../components/Layout';
+import { Button } from 'semantic-ui-react'
+import factory from '../../ethereum/factory'
+import web3 from '../../ethereum/web3'
 
 class CampaignShow extends Component {
+  state = {
+    loading: false
+  }
+
   render() {
-    return <h3>Campaign Show</h3>
+    return (
+      <Layout>
+      <h3>Campaign Details</h3>
+      <Button primary loading={this.state.loading}>View Requests</Button>
+      </Layout>
+    )
   }
 }
 
